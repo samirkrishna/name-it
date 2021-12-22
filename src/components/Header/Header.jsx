1ff4b0efc1113d503b87c73a430421569c993f88
Samir Krishna Cholleti <samirkrishna.cholleti@infor.com>
2021-12-22 21:11:59 +0530

    Added App and Header Components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
+        className="header-image"
+        alt=""
+      />
+      <h1 className="header-title">Name It</h1>
+    </div>
+  );
+};
+
+export default Header;
