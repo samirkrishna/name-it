be0a9707e07cd6b14fee25d39ecffd2e2a04cf6b
Samir Krishna Cholleti <samirkrishna.cholleti@infor.com>
2022-01-02 20:47:40 +0530

    Updated SearchBox Component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Header.css";
 
 const Header = (props) => {
@@ -8,7 +8,7 @@ const Header = (props) => {
       <img
         src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png"
         className="header-image"
-        alt=""
+        alt="icon"
       />
       <h1 className="header-title">{props.title}</h1>
       {/* <button onClick={()=>setState({props:"samir"})}>On click</button> */}
